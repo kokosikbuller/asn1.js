093b3c4e9b96f77c05402c45c28f42ab45797dde
kokosikbuller <kostyakrolll@gmail.com>
2024-02-20 14:45:02 +0200

    change buffer

--- test/use-test.js
+++ test/use-test.js
@@ -1,136 +1,123 @@
-'use strict';
+"use strict";
 /* global describe it */
 
-const assert = require('assert');
-const asn1 = require('..');
+const assert = require("assert");
+const asn1 = require("..");
 const bn = asn1.bignum;
-const fixtures = require('./fixtures');
+const fixtures = require("./fixtures");
 const jsonEqual = fixtures.jsonEqual;
 
-const Buffer = require('safer-buffer').Buffer;
+const Buffer = require("buffer/").Buffer;
 
-describe('asn1.js models', function() {
-  describe('plain use', function() {
-    it('should encode submodel', function() {
-      const SubModel = asn1.define('SubModel', function() {
-        this.seq().obj(
-          this.key('b').octstr()
-        );
+describe("asn1.js models", function () {
+  describe("plain use", function () {
+    it("should encode submodel", function () {
+      const SubModel = asn1.define("SubModel", function () {
+        this.seq().obj(this.key("b").octstr());
       });
-      const Model = asn1.define('Model', function() {
-        this.seq().obj(
-          this.key('a').int(),
-          this.key('sub').use(SubModel)
-        );
+      const Model = asn1.define("Model", function () {
+        this.seq().obj(this.key("a").int(), this.key("sub").use(SubModel));
       });
 
-      const data = {a: new bn(1), sub: {b: Buffer.from("XXX")}};
-      const wire = Model.encode(data, 'der');
-      assert.equal(wire.toString('hex'), '300a02010130050403585858');
-      const back = Model.decode(wire, 'der');
+      const data = { a: new bn(1), sub: { b: Buffer.from("XXX") } };
+      const wire = Model.encode(data, "der");
+      assert.equal(wire.toString("hex"), "300a02010130050403585858");
+      const back = Model.decode(wire, "der");
       jsonEqual(back, data);
     });
 
-    it('should honour implicit tag from parent', function() {
-      const SubModel = asn1.define('SubModel', function() {
-        this.seq().obj(
-          this.key('x').octstr()
-        )
+    it("should honour implicit tag from parent", function () {
+      const SubModel = asn1.define("SubModel", function () {
+        this.seq().obj(this.key("x").octstr());
       });
-      const Model = asn1.define('Model', function() {
+      const Model = asn1.define("Model", function () {
         this.seq().obj(
-          this.key('a').int(),
-          this.key('sub').use(SubModel).implicit(0)
+          this.key("a").int(),
+          this.key("sub").use(SubModel).implicit(0)
         );
       });
 
-      const data = {a: new bn(1), sub: {x: Buffer.from("123")}};
-      const wire = Model.encode(data, 'der');
-      assert.equal(wire.toString('hex'), '300a020101a0050403313233');
-      const back = Model.decode(wire, 'der');
+      const data = { a: new bn(1), sub: { x: Buffer.from("123") } };
+      const wire = Model.encode(data, "der");
+      assert.equal(wire.toString("hex"), "300a020101a0050403313233");
+      const back = Model.decode(wire, "der");
       jsonEqual(back, data);
     });
 
-    it('should honour explicit tag from parent', function() {
-      const SubModel = asn1.define('SubModel', function() {
-        this.seq().obj(
-          this.key('x').octstr()
-        )
+    it("should honour explicit tag from parent", function () {
+      const SubModel = asn1.define("SubModel", function () {
+        this.seq().obj(this.key("x").octstr());
       });
-      const Model = asn1.define('Model', function() {
+      const Model = asn1.define("Model", function () {
         this.seq().obj(
-          this.key('a').int(),
-          this.key('sub').use(SubModel).explicit(0)
+          this.key("a").int(),
+          this.key("sub").use(SubModel).explicit(0)
         );
       });
 
-      const data = {a: new bn(1), sub: {x: Buffer.from("123")}};
-      const wire = Model.encode(data, 'der');
-      assert.equal(wire.toString('hex'), '300c020101a00730050403313233');
-      const back = Model.decode(wire, 'der');
+      const data = { a: new bn(1), sub: { x: Buffer.from("123") } };
+      const wire = Model.encode(data, "der");
+      assert.equal(wire.toString("hex"), "300c020101a00730050403313233");
+      const back = Model.decode(wire, "der");
       jsonEqual(back, data);
-
     });
 
-    it('should get model with function call', function() {
-      const SubModel = asn1.define('SubModel', function() {
-        this.seq().obj(
-          this.key('x').octstr()
-        )
+    it("should get model with function call", function () {
+      const SubModel = asn1.define("SubModel", function () {
+        this.seq().obj(this.key("x").octstr());
       });
-      const Model = asn1.define('Model', function() {
+      const Model = asn1.define("Model", function () {
         this.seq().obj(
-          this.key('a').int(),
-          this.key('sub').use(function(obj) {
+          this.key("a").int(),
+          this.key("sub").use(function (obj) {
+            assert.equal(obj.a, 1);
+            return SubModel;
+          })
+        );
+      });
+
+      const data = { a: new bn(1), sub: { x: Buffer.from("123") } };
+      const wire = Model.encode(data, "der");
+      assert.equal(wire.toString("hex"), "300a02010130050403313233");
+      const back = Model.decode(wire, "der");
+      jsonEqual(back, data);
+    });
+
+    it("should get model with function call from choice", function () {
+      const SubModel = asn1.define("SubModel", function () {
+        this.seq().obj(this.key("x").octstr());
+      });
+      const Model = asn1.define("Model", function () {
+        this.seq().obj(
+          this.key("a").int(),
+          this.key("sub").choice({
+            variant1: this.use(function (obj) {
               assert.equal(obj.a, 1);
               return SubModel;
+            }),
           })
         );
       });
 
-      const data = {a: new bn(1), sub: {x: Buffer.from("123")}};
-      const wire = Model.encode(data, 'der');
-      assert.equal(wire.toString('hex'), '300a02010130050403313233');
-      const back = Model.decode(wire, 'der');
+      const data = {
+        a: new bn(1),
+        sub: { type: "variant1", value: { x: Buffer.from("123") } },
+      };
+      const wire = Model.encode(data, "der");
+      assert.equal(wire.toString("hex"), "300a02010130050403313233");
+      const back = Model.decode(wire, "der");
       jsonEqual(back, data);
-
     });
 
-    it('should get model with function call from choice', function() {
-      const SubModel = asn1.define('SubModel', function() {
-        this.seq().obj(
-          this.key('x').octstr()
-        )
-      });
-      const Model = asn1.define('Model', function() {
-        this.seq().obj(
-          this.key('a').int(),
-          this.key('sub').choice({
-            variant1: this.use(function(obj) {
-                assert.equal(obj.a, 1);
-                return SubModel;
-            })
-          })
-        );
-      });
-
-      const data = {a: new bn(1), sub: {type: 'variant1', value: {x: Buffer.from("123")}}};
-      const wire = Model.encode(data, 'der');
-      assert.equal(wire.toString('hex'), '300a02010130050403313233');
-      const back = Model.decode(wire, 'der');
-      jsonEqual(back, data);
-
-    });
-
-    it('should support recursive submodels', function() {
-      const PlainSubModel = asn1.define('PlainSubModel', function() {
+    it("should support recursive submodels", function () {
+      const PlainSubModel = asn1.define("PlainSubModel", function () {
         this.int();
       });
-      const RecursiveModel = asn1.define('RecursiveModel', function() {
+      const RecursiveModel = asn1.define("RecursiveModel", function () {
         this.seq().obj(
-          this.key('plain').bool(),
-          this.key('content').use(function(obj) {
-            if(obj.plain) {
+          this.key("plain").bool(),
+          this.key("content").use(function (obj) {
+            if (obj.plain) {
               return PlainSubModel;
             } else {
               return RecursiveModel;
@@ -140,18 +127,16 @@ describe('asn1.js models', function() {
       });
 
       const data = {
-        'plain': false,
-        'content': {
-          'plain': true,
-          'content': new bn(1)
-        }
+        plain: false,
+        content: {
+          plain: true,
+          content: new bn(1),
+        },
       };
-      const wire = RecursiveModel.encode(data, 'der');
-      assert.equal(wire.toString('hex'), '300b01010030060101ff020101');
-      const back = RecursiveModel.decode(wire, 'der');
+      const wire = RecursiveModel.encode(data, "der");
+      assert.equal(wire.toString("hex"), "300b01010030060101ff020101");
+      const back = RecursiveModel.decode(wire, "der");
       jsonEqual(back, data);
     });
-
   });
 });
-
